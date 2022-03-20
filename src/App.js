9942a492092c42775782cbcc9f42781efcb33356
omokhele <anne.imomoh@gmail.com>
2022-03-20 12:08:27 -0700

    Added css styling to the js file.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import ContactList from './components/ContactList';
 
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,15 +4,18 @@ import contact from "../data/contact.json"
 
 function ContactList () {
     return(
-        <div>
+        <div className="outerDiv">
             {
                     contact.map((contact, index) => {
                             return(
-                                <div>
-                                      <img src= {contact.picture.thumbnail} />
+                                <div className="innerDiv">
+                                    <img className="image" src= {contact.picture.thumbnail}/>
+                                    <div className="innerMostDiv">
+                                      
                                       <h3> {contact.name.first} {contact.name.last}</h3>
                                       <p>Home: {contact.phone}</p>
-                                      <p>Mobile: {contact.cell}</p>                       
+                                      <p>Mobile: {contact.cell}</p> 
+                                      </div>                      
                                 </div>
                                  )
                     })
